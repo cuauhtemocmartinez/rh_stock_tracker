1a7c6dca4d681b37e42929d21f8beaebe3524902
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-05-10 13:28:26 -0700

    added functions

--- ema_tracker.py
+++ ema_tracker.py
@@ -9,4 +9,19 @@ def calculate_ema(prices, days, smoothing=2):
 
 
 symbol = 'MSFT'
-df = web.DataReader(symbol, 'yahoo', '2015-01-01', '2016-01-01')
+df = web.DataReader(symbol, 'yahoo', '2021-01-01', '2016-05-10')
+
+
+def calculate_ema(prices, days, smoothing=2):
+    ema = [sum(prices[:days]) / days]  # First method
+    ################################################
+    ema = []
+    ema.append(sum(prices[:days]) / days)  # Second method
+
+
+def calculate_ema(prices, days, smoothing=2):
+    ema = [sum(prices[:days]) / days]
+    for price in prices[days:]:
+        ema.append((price * (smoothing / (1 + days))) +
+                   ema[-1] * (1 - (smoothing / (1 + days))))
+    return ema

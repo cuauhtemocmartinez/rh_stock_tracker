be84df424cb5c7078dfe9988fbd927f644c71a78
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-05-10 13:30:17 -0700

    added plots

--- ema_tracker.py
+++ ema_tracker.py
@@ -5,23 +5,29 @@ import pandas_datareader as web
 
 
 def calculate_ema(prices, days, smoothing=2):
-    pass
-
-
-symbol = 'MSFT'
-df = web.DataReader(symbol, 'yahoo', '2021-01-01', '2016-05-10')
-
-
-def calculate_ema(prices, days, smoothing=2):
-    ema = [sum(prices[:days]) / days]  # First method
-    ################################################
-    ema = []
-    ema.append(sum(prices[:days]) / days)  # Second method
-
-
-def calculate_ema(prices, days, smoothing=2):
     ema = [sum(prices[:days]) / days]
     for price in prices[days:]:
         ema.append((price * (smoothing / (1 + days))) +
                    ema[-1] * (1 - (smoothing / (1 + days))))
     return ema
+
+
+symbol = 'MSFT'
+df = web.DataReader(symbol, 'yahoo', '2015-01-01', '2016-01-01')
+
+# Add this line to save EMA values in a list
+ema = calculate_ema(df['Close'], 10)
+
+# Creates array [0, 1, 2, 3, ..., df.shape[0]]
+price_X = np.arange(df.shape[0])
+# Creates array [10, 11, 12, 13, ..., df.shape[0]+1]
+ema_X = np.arange(10, df.shape[0]+1)
+# We start at 10, because we use the first 10 values to calculate the SMA,
+# then we calculate EMA form the 11th value
+
+plt.xlabel('Days')
+plt.ylabel('Price')
+plt.plot(price_X, df['Close'], label='Closing Prices')
+plt.plot(ema_X, ema, label='EMA')
+plt.legend()
+plt.show()

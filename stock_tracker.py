de669339d54f4b3358447607829e47844ef10928
cuauhtemocmartinez <mr.cuauhtemocmartinez@gmail.com>
2021-06-08 20:23:45 -0700

    created login function

--- stock_tracker.py
+++ stock_tracker.py
@@ -3,3 +3,14 @@ import robin_stocks as rh
 import dateime as dt
 import time
 
+def login(days):
+    time_logged_in = 60*60*24*days
+    rh.authentication.login(username=config.USERNAME,
+                            password=config.PASSWORD,
+                            expiresIn=time_logged_in,
+                            scope+'internal',
+                            by_sms=True,
+                            store_session=True)
+
+def logout();
+    
